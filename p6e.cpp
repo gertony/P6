0998626f04971e8768c8c4bd17a73151c7cb1af6
Tony Jimenez <84590027+gertony@users.noreply.github.com>
2023-05-09 21:08:51 -0500

    actualizacion ejercicio 3
    acepta pancito

--- p6e.cpp
+++ p6e.cpp
@@ -3,10 +3,29 @@
 #include<vector>
 using namespace std;
 class Alumno{
+        public:
         string nombre;
         int edad;
         double promedio;
+        Alumno(string nombre, int edad, double promedio){
+                this->nombre = nombre;
+                this->edad = edad;
+                this->promedio = promedio;
+              
+        }
+        friend ostream& operator<<(ostream& os, const Alumno& alumno);  //declaración de las funciones como amigas
+        friend bool operator <(const Alumno&alumno1, const Alumno&alumno2);
 };
+
+ ostream& operator <<(ostream& os, const Alumno& alumno) { //Sobrecarga del operador "<<" para poder usarlo para imprimir el contenido de los objetos
+        os << "[" << alumno.nombre << ", " << alumno.edad << ", " << alumno.promedio<<"]"<<endl;
+        return os;
+    }
+
+ bool operator <(const Alumno&alumno1, const Alumno&alumno2){//Sobrecarga del operador ">" para poder comparar el valor del atributo promedio de una clase con el de otra
+        return alumno1.promedio > alumno2.promedio;
+}
+
 class Grupo{
         vector<Alumno>alumnos;
         int cantidad;
